8795909ad4eca36ec63f31e9e6c51cc713576df9
watchdan <daankuiperdenhaag@gmail.com>
2015-02-18 20:21:49 +0100

    Just a f*cking simple program.
    To show off my skills.

---
+++ CyclistQuizOriginalv1.py
@@ -0,0 +1,6 @@
+#This is just a test program.
+name = raw_input("Yo name is?")
+bike = raw_input("What bicycle do yo ride?")
+speed = raw_input("Do you cycle fast or slow?")
+statement = "Okay, %s, you're a %s cyclist on a %s ? Good for you, but you'll never beat Lauro!" % (name, speed, bike)
+print statement
